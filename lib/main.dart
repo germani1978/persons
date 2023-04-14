6588bc71e8dd116c3d0104945cc539aad6b81edf
germani <germanibenitezalfonso1@gmail.com>
2023-04-13 20:09:28 -0400

    onchange

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-
-void  main(List<String> args) {
-    runApp( MyApp() );
+void main(List<String> args) {
+  runApp(MyApp());
 }
 
 class Persona {
-  final String nombre;
-  final int Publicaciones;
-  final int Videos;
-  final int Horas;
-  final int Revisitas;
-  final int Estudios;
+  String nombre;
+  int Publicaciones;
+  int Videos;
+  int Horas;
+  int Revisitas;
+  int Estudios;
 
   Persona(
       {required this.nombre,
@@ -25,130 +25,153 @@ class Persona {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'App para recopilar datos',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Time'),
           actions: [
-            IconButton(
-              onPressed: (){}, 
-              icon: const Icon(Icons.settings)
-            ),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
           ],
         ),
-        body: Center(
-         
+        body: SingleChildScrollView(
+          child: ListaPersonas(),
+          scrollDirection: Axis.horizontal,
         ),
       ),
     );
   }
 }
 
-class ListaPersonas extends StatelessWidget {
-   ListaPersonas({super.key});
+class ListaPersonas extends StatefulWidget {
+  ListaPersonas({super.key});
+
+  @override
+  State<ListaPersonas> createState() => _ListaPersonasState();
+}
+
+class _ListaPersonasState extends State<ListaPersonas> {
 
   final List<Persona> listaPersonas = [
-    Persona(nombre: 'Juan', Publicaciones: 10, Videos: 20, Horas: 50, Revisitas: 40, Estudios: 34),
-    Persona(nombre: 'Pedro', Publicaciones: 10, Videos: 20, Horas: 50, Revisitas: 40, Estudios: 34),
-    Persona(nombre: 'Silvia', Publicaciones: 10, Videos: 20, Horas: 50, Revisitas: 40, Estudios: 34),
-    Persona(nombre: 'Ernesto', Publicaciones: 10, Videos: 20, Horas: 50, Revisitas: 40, Estudios: 34),
-    Persona(nombre: 'Fidel', Publicaciones: 10, Videos: 20, Horas: 50, Revisitas: 40, Estudios: 34),
-    Persona(nombre: 'Juana', Publicaciones: 10, Videos: 20, Horas: 50, Revisitas: 40, Estudios: 34),
+    Persona(
+        nombre: 'Juan',
+        Publicaciones: 10,
+        Videos: 20,
+        Horas: 50,
+        Revisitas: 40,
+        Estudios: 34),
+    Persona(
+        nombre: 'Pedro',
+        Publicaciones: 10,
+        Videos: 20,
+        Horas: 50,
+        Revisitas: 40,
+        Estudios: 34),
+    Persona(
+        nombre: 'Silvia',
+        Publicaciones: 10,
+        Videos: 20,
+        Horas: 50,
+        Revisitas: 40,
+        Estudios: 34),
+    Persona(
+        nombre: 'Ernesto',
+        Publicaciones: 10,
+        Videos: 20,
+        Horas: 50,
+        Revisitas: 40,
+        Estudios: 34),
+    Persona(
+        nombre: 'Fidel',
+        Publicaciones: 10,
+        Videos: 20,
+        Horas: 50,
+        Revisitas: 40,
+        Estudios: 34),
+    Persona(
+        nombre: 'Juana',
+        Publicaciones: 10,
+        Videos: 20,
+        Horas: 50,
+        Revisitas: 40,
+        Estudios: 34),
   ];
 
   @override
   Widget build(BuildContext context) {
     return DataTable(
-      columns: [ 
+      columns: [
         DataColumn(label: Text('Nombre')),
         DataColumn(label: Text('Publicaciones')),
         DataColumn(label: Text('Videos')),
         DataColumn(label: Text('Horas')),
         DataColumn(label: Text('Revisitas')),
         DataColumn(label: Text('Estudios')),
-      ], 
-      rows: listaPersonas.map((persona) {
-        return DataRow(cells: [
-          DataCell(Text(persona.nombre)),
-          DataCell(Text(persona.Publicaciones.toString())),
-          DataCell(Text(persona.Videos.toString())),
-          DataCell(Text(persona.Horas.toString())),
-          DataCell(Text(persona.Revisitas.toString())),
-          DataCell(Text(persona.Estudios.toString())),
-        ]);
-      }).toList(),
-    );
-  }
-}
-
-
-
-/**
- * 
- * import 'package:flutter/material.dart';
-
-class ListaPersonas extends StatelessWidget {
-  final List<Persona> listaPersonas;
-
-  ListaPersonas(this.listaPersonas);
-
-  @override
-  Widget build(BuildContext context) {
-    return DataTable(
-      columns: [
-        DataColumn(label: Text('Foto')),
-        DataColumn(label: Text('Nombre')),
-        DataColumn(label: Text('Edad')),
-        DataColumn(label: Text('Peso')),
-        DataColumn(label: Text('Altura')),
       ],
       rows: listaPersonas.map((persona) {
         return DataRow(cells: [
-          DataCell(CircleAvatar(backgroundImage: NetworkImage(persona.foto))),
-          DataCell(Text(persona.nombre)),
-          DataCell(Text(persona.edad.toString())),
-          DataCell(Text('${persona.peso} kg')),
-          DataCell(Text('${persona.altura} m')),
+
+          DataCell(TextFormField(
+            initialValue: persona.nombre,
+            onChanged: (value) => {persona.nombre = value},
+          )),
+          DataCell(TextFormField(
+            initialValue: persona.Publicaciones.toString(),
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            onChanged: (value)  {
+               setState(() {
+                 persona.Publicaciones =  value.isEmpty ? 0 :  int.parse(value);
+               });
+            },
+          )),
+          DataCell(TextFormField(
+            initialValue: persona.Videos.toString(),
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            onChanged: (value) {
+              setState(() {
+                persona.Videos =  value.isEmpty ? 0 :  int.parse(value);
+              });
+            },
+          )),
+          DataCell(TextFormField(
+            initialValue: persona.Horas.toString(),
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            onChanged: (value) {
+              setState(() {
+                persona.Horas =  value.isEmpty ? 0 :  int.parse(value);
+              });
+            },
+          )),
+          DataCell(TextFormField(
+            initialValue: persona.Revisitas.toString(),
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            onChanged: (value)  {
+              setState(() {
+               persona.Revisitas =  value.isEmpty ? 0 :  int.parse(value);
+              });
+            },
+          )),
+          DataCell(TextFormField(
+            initialValue: persona.Estudios.toString(),
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            onChanged: (value) { 
+              setState(() {
+                persona.Estudios =  value.isEmpty ? 0 :  int.parse(value);
+              });
+            },
+          )),
         ]);
       }).toList(),
     );
   }
 }
 
-class Persona {
-  String nombre;
-  int edad;
-  double peso;
-  double altura;
-  String foto;
-
-  Persona(this.nombre, this.edad, this.peso, this.altura, this.foto);
-}
-
-void main() {
-  List<Persona> listaPersonas = [
-    Persona('Juan', 25, 70, 1.75, 'https://miweb.com/foto1.jpg'),
-    Persona('María', 30, 60, 1.65, 'https://miweb.com/foto2.jpg'),
-    Persona('Pedro', 28, 80, 1.80, 'https://miweb.com/foto3.jpg'),
-  ];
-
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Lista de personas')),
-        body: ListaPersonas(listaPersonas),
-      ),
-    ),
-  );
-}
-
- */
