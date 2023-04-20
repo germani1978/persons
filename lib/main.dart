6f307f6b5c9bba2d515b9c894e80e12cfa5d6b6c
germani <germanibenitezalfonso1@gmail.com>
2023-04-20 14:18:35 -0400

    controller a campos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -7,19 +7,19 @@ void main(List<String> args) {
 
 class Persona {
   String nombre;
-  int Publicaciones;
-  int Videos;
-  int Horas;
-  int Revisitas;
-  int Estudios;
+  int publicaciones;
+  int videos;
+  int horas;
+  int revisitas;
+  int estudios;
 
   Persona(
       {required this.nombre,
-      required this.Publicaciones,
-      required this.Videos,
-      required this.Horas,
-      required this.Revisitas,
-      required this.Estudios});
+      required this.publicaciones,
+      required this.videos,
+      required this.horas,
+      required this.revisitas,
+      required this.estudios});
 }
 
 class MyApp extends StatelessWidget {
@@ -47,6 +47,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class ListaPersonas extends StatefulWidget {
   ListaPersonas({super.key});
 
@@ -55,123 +56,175 @@ class ListaPersonas extends StatefulWidget {
 }
 
 class _ListaPersonasState extends State<ListaPersonas> {
-
+  
   final List<Persona> listaPersonas = [
     Persona(
         nombre: 'Juan',
-        Publicaciones: 10,
-        Videos: 20,
-        Horas: 50,
-        Revisitas: 40,
-        Estudios: 34),
+        publicaciones: 10,
+        videos: 20,
+        horas: 50,
+        revisitas: 40,
+        estudios: 34),
     Persona(
         nombre: 'Pedro',
-        Publicaciones: 10,
-        Videos: 20,
-        Horas: 50,
-        Revisitas: 40,
-        Estudios: 34),
+        publicaciones: 10,
+        videos: 20,
+        horas: 50,
+        revisitas: 40,
+        estudios: 34),
     Persona(
         nombre: 'Silvia',
-        Publicaciones: 10,
-        Videos: 20,
-        Horas: 50,
-        Revisitas: 40,
-        Estudios: 34),
+        publicaciones: 10,
+        videos: 20,
+        horas: 50,
+        revisitas: 40,
+        estudios: 34),
     Persona(
         nombre: 'Ernesto',
-        Publicaciones: 10,
-        Videos: 20,
-        Horas: 50,
-        Revisitas: 40,
-        Estudios: 34),
+        publicaciones: 10,
+        videos: 20,
+        horas: 50,
+        revisitas: 40,
+        estudios: 34),
     Persona(
         nombre: 'Fidel',
-        Publicaciones: 10,
-        Videos: 20,
-        Horas: 50,
-        Revisitas: 40,
-        Estudios: 34),
+        publicaciones: 10,
+        videos: 20,
+        horas: 50,
+        revisitas: 40,
+        estudios: 34),
     Persona(
         nombre: 'Juana',
-        Publicaciones: 10,
-        Videos: 20,
-        Horas: 50,
-        Revisitas: 40,
-        Estudios: 34),
+        publicaciones: 10,
+        videos: 20,
+        horas: 50,
+        revisitas: 40,
+        estudios: 34),
   ];
+
+  final List<TextEditingController> nombreControllers = [];
+  final List<TextEditingController> publicacionesControllers = [];
+  final List<TextEditingController> videosControllers = [];
+  final List<TextEditingController> horasControllers = [];
+  final List<TextEditingController> revisitasControllers = [];
+  final List<TextEditingController> estudiosControllers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    listaPersonas.forEach((persona) {
+      nombreControllers.add(TextEditingController(text: persona.nombre));
+      publicacionesControllers
+          .add(TextEditingController(text: persona.publicaciones.toString()));
+      videosControllers
+          .add(TextEditingController(text: persona.videos.toString()));
+      horasControllers
+          .add(TextEditingController(text: persona.horas.toString()));
+      revisitasControllers
+          .add(TextEditingController(text: persona.revisitas.toString()));
+      estudiosControllers
+          .add(TextEditingController(text: persona.estudios.toString()));
+    });
+  }
+
+  @override
+  void dispose() {
+    nombreControllers.forEach((controller) => controller.dispose());
+    publicacionesControllers.forEach((controller) => controller.dispose());
+    videosControllers.forEach((controller) => controller.dispose());
+    horasControllers.forEach((controller) => controller.dispose());
+    revisitasControllers.forEach((controller) => controller.dispose());
+    estudiosControllers.forEach((controller) => controller.dispose());
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: [
-        DataColumn(label: Text('Nombre')),
-        DataColumn(label: Text('Publicaciones')),
-        DataColumn(label: Text('Videos')),
-        DataColumn(label: Text('Horas')),
-        DataColumn(label: Text('Revisitas')),
-        DataColumn(label: Text('Estudios')),
-      ],
-      rows: listaPersonas.map((persona) {
-        return DataRow(cells: [
-
-          DataCell(TextFormField(
-            initialValue: persona.nombre,
-            onChanged: (value) => {persona.nombre = value},
-          )),
-          DataCell(TextFormField(
-            initialValue: persona.Publicaciones.toString(),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            onChanged: (value)  {
-               setState(() {
-                 persona.Publicaciones =  value.isEmpty ? 0 :  int.parse(value);
-               });
-            },
-          )),
-          DataCell(TextFormField(
-            initialValue: persona.Videos.toString(),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            onChanged: (value) {
-              setState(() {
-                persona.Videos =  value.isEmpty ? 0 :  int.parse(value);
-              });
-            },
-          )),
-          DataCell(TextFormField(
-            initialValue: persona.Horas.toString(),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            onChanged: (value) {
-              setState(() {
-                persona.Horas =  value.isEmpty ? 0 :  int.parse(value);
-              });
-            },
-          )),
-          DataCell(TextFormField(
-            initialValue: persona.Revisitas.toString(),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            onChanged: (value)  {
-              setState(() {
-               persona.Revisitas =  value.isEmpty ? 0 :  int.parse(value);
-              });
-            },
-          )),
-          DataCell(TextFormField(
-            initialValue: persona.Estudios.toString(),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            onChanged: (value) { 
-              setState(() {
-                persona.Estudios =  value.isEmpty ? 0 :  int.parse(value);
-              });
-            },
-          )),
-        ]);
-      }).toList(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de personas'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            DataTable(
+              columns: [
+                DataColumn(label: Text('Nombre')),
+                DataColumn(label: Text('publicaciones')),
+                DataColumn(label: Text('videos')),
+                DataColumn(label: Text('horas')),
+                DataColumn(label: Text('revisitas')),
+                DataColumn(label: Text('estudios')),
+              ],
+              rows: listaPersonas.map((persona) {
+                int index = listaPersonas.indexOf(persona);
+                nombreControllers.add(TextEditingController(text: persona.nombre));
+                publicacionesControllers.add(TextEditingController(text: persona.publicaciones.toString()));
+                videosControllers.add(TextEditingController(text: persona.videos.toString()));
+                horasControllers.add(TextEditingController(text: persona.horas.toString()));
+                revisitasControllers.add(TextEditingController(text: persona.revisitas.toString()));
+                estudiosControllers.add(TextEditingController(text: persona.estudios.toString()));
+                return DataRow(cells: [
+                  DataCell(TextField(
+                    controller: nombreControllers[index],
+                    decoration: InputDecoration(
+                      hintText: 'Nombre',
+                    ),
+                  )),
+                  DataCell(TextField(
+                    controller: publicacionesControllers[index],
+                    decoration: InputDecoration(
+                      hintText: 'publicaciones',
+                    ),
+                  )),
+                  DataCell(TextField(
+                    controller: videosControllers[index],
+                    decoration: InputDecoration(
+                      hintText: 'videos',
+                    ),
+                  )),
+                  DataCell(TextField(
+                    controller: horasControllers[index],
+                    decoration: InputDecoration(
+                      hintText: 'horas',
+                    ),
+                  )),
+                  DataCell(TextField(
+                    controller: revisitasControllers[index],
+                    decoration: InputDecoration(
+                      hintText: 'revisitas',
+                    ),
+                  )),
+                  DataCell(TextField(
+                    controller: estudiosControllers[index],
+                    decoration: InputDecoration(
+                      hintText: 'estudios',
+                    ),
+                  )),
+                ]);
+              }).toList(),
+            ),
+            ElevatedButton(
+              child: Text('Guardar cambios'),
+              onPressed: () {
+                for (int i = 0; i < listaPersonas.length; i++) {
+                  listaPersonas[i].nombre = nombreControllers[i].text;
+                  listaPersonas[i].publicaciones = int.parse(publicacionesControllers[i].text);
+                  listaPersonas[i].videos = int.parse(videosControllers[i].text);
+                  listaPersonas[i].horas = int.parse(horasControllers[i].text);
+                  listaPersonas[i].revisitas = int.parse(revisitasControllers[i].text);
+                  listaPersonas[i].estudios = int.parse(estudiosControllers[i].text);
+                }
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Cambios guardados')),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
 
